ce0ed024c2be17256a1a7a6b060f43b9cb61ad0f
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2024-05-16 23:38:49 -0400

    Update TawkToChat.js

--- src/components/TawkToChat.js
+++ src/components/TawkToChat.js
@@ -4,7 +4,7 @@ const TawkToChat = () => {
   useEffect(() => {
     const s1 = document.createElement("script");
     s1.async = true;
-    s1.src = process.env.POSTMARK_API_KEY;
+    s1.src = process.env.TAWK_TO_KEY;
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     document.head.appendChild(s1);
